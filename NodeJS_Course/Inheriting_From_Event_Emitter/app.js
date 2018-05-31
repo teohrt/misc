47ac25c4cee3f66dad35459c1644552a46c020c4
Trace Ohrt <teohrt18@gmail.com>
2018-05-30 22:43:02 -0500

    syntactic sugar with classes

--- NodeJS_Course/Inheriting_From_Event_Emitter/app.js
+++ NodeJS_Course/Inheriting_From_Event_Emitter/app.js
@@ -1,25 +1,21 @@
+'use strict';
 var EventEmitter = require('events');
-var util = require('util');
 
-function Greetr() {
-    // Super constructor: the constructor I'm inheriting from!
-    // This gives things the object has outside of it's prototype
-    EventEmitter.call(this);
-    this.greeting = 'Hello world!';
+// syntactic sugar
+
+// sets up the prototype chain. Greetr inherits from EventEmitter
+class Greetr extends EventEmitter{
+    constructor() {
+        super();
+        this.greeting = 'Hello world!';
+    }
+
+    // This gives the function to Greetr.prototype.greet like before
+    greet(data) {
+        console.log(this.greeting + ': ' + data);
+        this.emit('greet', data);
+    }
 }
-
-// any new objects created from Greetr will 
-// also get access to methods and properties on the
-// prototype of EventEmitter!
-// JUST PROTOTYPE
-util.inherits(Greetr, EventEmitter);
-
-// an example of passing data through events 
-Greetr.prototype.greet = function(data) {
-    console.log(this.greeting + ': ' + data);
-    this.emit('greet', data);
-    // emits to any listeners
-};
 
 var greeter1 = new Greetr();
 
