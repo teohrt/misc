94e0e0deff72605a0e7c64a48c99b59d90894754
Trace Ohrt <teohrt18@gmail.com>
2018-06-04 04:37:12 -0500

    experimenting with html interpretation

--- NodeJS_Course/HelloWorld!/app.js
+++ NodeJS_Course/HelloWorld!/app.js
@@ -2,6 +2,7 @@
 
 // getting this from the node.js core
 var http = require('http');
+var fs = require('fs');
 
 // creating an object and getting a callback
 // which is actually an event listener
@@ -11,9 +12,11 @@ http.createServer(function(req, res) {
     // the sending of information back down the stream
     
     // header
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     // body
-    res.end('Hello World\n');
+    var html = fs.readFileSync(__dirname + '/index.html')
+    res.end(html);
+
     // node.js takes this and creates 
     // a properly formatted http response
 
