f956cf2dae31860574eac1529f043605d5eec096
Trace Ohrt <teohrt18@gmail.com>
2018-06-15 17:41:03 -0500

    Basic routing examples

---
+++ NodeJS_Course/Routing/app.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+
+  if (req.url === '/') {
+    fs.createReadStream(__dirname + '/index.htm').pipe(res);
+  }
+
+  else if (req.url === '/api') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var obj = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
+  }
+
+  else {  
+    //if something doesn't exist... 404 error
+    res.writeHead(404);
+    res.end();
+  }
+
+}).listen(8080, '127.0.0.1');
