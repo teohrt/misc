fb29e3a97b084ecfa044875d435a02eafa246244
Trace Ohrt <teohrt18@gmail.com>
2018-09-05 01:24:21 -0500

    Comments

--- Golang_Course/HELLOWORLD/main.go
+++ Golang_Course/HELLOWORLD/main.go
@@ -1,7 +1,13 @@
+// Compiled an excecutable binary file with the command:
+// "go build main.go"
+
+// The name "main" makes the package executable
 package main
 
+// Imports the standard format library
 import "fmt"
 
+// Every excecutable package needs a function called "main"
 func main() {
 	fmt.Println("Hello world!")
 }
