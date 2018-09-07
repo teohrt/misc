43e61bed04edfdaa2b1bac580645ef66b0e120ed
Trace Ohrt <teohrt18@gmail.com>
2018-09-07 17:17:50 -0500

    Experimenting with creating types

--- Golang_Course/CARDS/main.go
+++ Golang_Course/CARDS/main.go
@@ -1,18 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
 	// This is going to be a slice of type string
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 
 	// This does not modify the slice. It returns a new one!
 	cards = append(cards, "Six of Spades")
 
-	// Iterate over cards
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	// Print cards
+	cards.print()
 }
 
 // Seperate function returning a string
