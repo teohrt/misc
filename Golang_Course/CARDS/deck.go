54329e07538c7a50d5ec15cb967aca189d30425a
Trace Ohrt <teohrt18@gmail.com>
2018-09-10 10:08:23 -0500

    Multiple return values!

--- Golang_Course/CARDS/deck.go
+++ Golang_Course/CARDS/deck.go
@@ -11,9 +11,9 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Hearts", "Diamonds", "Spades", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "JACK", "QUEEN", "KING"}
 
-	// The underscore is to handle required unnused variables
+	// The underscore is to handle required unnused variable errors
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
@@ -27,4 +27,13 @@ func (d deck) print() {
 	for index, card := range d {
 		fmt.Println(index, card)
 	}
+}
+
+// Returning two values. Both of type deck
+func deal(d deck, handSize int) (deck, deck) {
+	// d[startIndexIncluding : upToNotIncluding]
+	// If left side is blank, Go assumes the beginning
+	// If right side is blank, Go assumes the end
+
+	return d[:handSize], d[handSize:]
 }
--- Golang_Course/CARDS/main.go
+++ Golang_Course/CARDS/main.go
@@ -4,6 +4,10 @@ func main() {
 	// This is going to be a slice of type string
 	cards := newDeck()
 
-	// Print cards
-	cards.print()
+	// Multiple return values!
+	hand, remainingDeck := deal(cards, 5)
+
+	hand.print()
+	remainingDeck.print()
+
 }
