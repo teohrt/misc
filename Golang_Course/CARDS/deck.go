2d6adb3251d5cdfcd774417ab69cbf2f36a3a165
Trace Ohrt <teohrt18@gmail.com>
2018-09-10 09:43:29 -0500

    Created the newDeck() function

--- Golang_Course/CARDS/deck.go
+++ Golang_Course/CARDS/deck.go
@@ -6,6 +6,23 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+// Create and return a list of playing cards
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Hearts", "Diamonds", "Spades", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// The underscore is to handle required unnused variables
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for index, card := range d {
 		fmt.Println(index, card)
--- Golang_Course/CARDS/main.go
+++ Golang_Course/CARDS/main.go
@@ -2,16 +2,8 @@ package main
 
 func main() {
 	// This is going to be a slice of type string
-	cards := deck{"Ace of Diamonds", newCard()}
-
-	// This does not modify the slice. It returns a new one!
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	// Print cards
 	cards.print()
-}
-
-// Seperate function returning a string
-func newCard() string {
-	return "Five of Diamonds"
 }
