5e86df6f3e13653a457a42b6f7967cf975012399
Trace Ohrt <teohrt18@gmail.com>
2018-09-11 21:50:32 -0500

    Map iteration

--- Golang_Course/MAP/main.go
+++ Golang_Course/MAP/main.go
@@ -1,23 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	// Much like structs there are a few different ways to create maps
 
 	// var colors map[string]string
 
-	//colors := make(map[string]string)
+	// colors := make(map[string]string)
 
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#008000",
 	}
+	// Adding a key value pair:
 	colors["white"] = "#ffffff"
-	delete(colors, "red")
 
-	fmt.Println(colors)
+	// Deleting a pair:
+	// delete(colors, "red")
 
+	printMap(colors)
+
+}
+
+// Iterating over a map
+func printMap(c map[string]string) {
+	for key, value := range c {
+		fmt.Println("Hex code for", key, "is", value)
+	}
 }
