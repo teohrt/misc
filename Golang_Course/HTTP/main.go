15acbe338d5a33a8215427a05d5ad836fd8ffe47
Trace Ohrt <teohrt18@gmail.com>
2018-09-12 17:55:50 -0500

    Interface experimentation

--- Golang_Course/HTTP/main.go
+++ Golang_Course/HTTP/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,7 +16,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	// Same output:
+	// Utilizes writer interface
+	// io.Copy(os.Stdout, resp.Body)
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
+
+// Custom write function implementing the write interface!
+// By just defining the function and associating it
+// with logWriter, logWriter is implementing the writer
+// interface
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
