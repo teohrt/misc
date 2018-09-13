ec932d6c61a75110b47c5381a232eddee9107b03
Trace Ohrt <teohrt18@gmail.com>
2018-09-13 15:43:54 -0500

    Blocking channels and receiving messages

--- Golang_Course/CHANNELS/main.go
+++ Golang_Course/CHANNELS/main.go
@@ -14,18 +14,28 @@ func main() {
 		"http://facebook.com",
 	}
 
-	// Each requests must wait for the previous one to complete
+	// Create a channel to pass into our function
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	// THE WHOLE LOOP IS NOT EXCECUTED ALL AT ONCE
+	// <-c is a blocking call
+	// This means that when the program gets to it, it temporarily pauses
+	// And waits for another go routine to send a message over the channel
+	// This is so cool!
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
-		fmt.Println(link, "might be down!")
+		c <- "DOWN! : " + link
 		return
 	}
-
-	fmt.Println(link, "is up!")
+	c <- "UP AND RUNNING!: " + link
 }
