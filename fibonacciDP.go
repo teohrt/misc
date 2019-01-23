b6aaac13a7882692d04a3bcc0e1f8d1cebf7802a
Trace Ohrt <teohrt18@gmail.com>
2019-01-23 01:53:44 -0600

    iterative solution with memoization

--- fibonacciDP.go
+++ fibonacciDP.go
@@ -9,20 +9,42 @@ import (
 /*
 n  : 0, 1, 2, 3, 4, 5, 6, 7,  ...
 fib: 0, 1, 1, 2, 3, 5, 8, 13, ...
+
+This code takes an integer from user input and
+calculates the Nth Fibonacci number using 3 approches:
+
+Iteratively with memoization,
+Recursively with memoization,
+and the naive recursive solution
 */
 
 func main() {
 	memo := make(map[int]int)
 	n, _ := strconv.Atoi(os.Args[1])
 
-	//fmt.Println(fibNaive(n))
-	fmt.Println(fibDP(n, memo))
+	fmt.Println(recursiveFibDP(n, memo))
+	fmt.Println(iterativeFibDP(n))
+	fmt.Println(fibNaive(n))
+}
+
+// Iteratively return the Nth Fibonacci number utilizing memoization!!
+// Time complexity : O(n):
+// Space complexity: O(n):
+func iterativeFibDP(n int) int {
+	var slice = make([]int, n+1)
+	slice[0] = 0
+	slice[1] = 1
+	for i := 2; i <= n; i++ {
+		slice[i] = slice[i-1] + slice[i-2]
+	}
+
+	return slice[n]
 }
 
 // Recursively returns the Nth Fibonacci number utilizing memoization!!
 // Time  complexity: O(n)
 // Space complexity: O(n)
-func fibDP(n int, memo map[int]int) int {
+func recursiveFibDP(n int, memo map[int]int) int {
 	// Check if the value is in the memo
 	value, found := memo[n]
 	if found {
@@ -31,14 +53,14 @@ func fibDP(n int, memo map[int]int) int {
 	if n <= 1 {
 		value = n
 	} else {
-		value = fibDP(n-1, memo) + fibDP(n-2, memo)
+		value = recursiveFibDP(n-1, memo) + recursiveFibDP(n-2, memo)
 	}
 
 	memo[n] = value
 	return value
 }
 
-// Naive solution
+// Naive solution:
 // Time  Complexity: O(2^n)
 // Space Complexity: O(n)
 func fibNaive(n int) int {
