1e8e070c4625072762c7107d02961b57facbc39c
Trace Ohrt <teohrt18@gmail.com>
2019-01-29 01:33:16 -0600

    added excecution time decorator functionality

--- fibonacciDP.go
+++ fibonacciDP.go
@@ -3,28 +3,45 @@ package main
 import (
 	"fmt"
 	"os"
+	"reflect"
+	"runtime"
 	"strconv"
+	"time"
 )
 
 /*
 n  : 0, 1, 2, 3, 4, 5, 6, 7,  ...
 fib: 0, 1, 1, 2, 3, 5, 8, 13, ...
 
-This code takes an integer from user input and
-calculates the Nth Fibonacci number using 3 approches:
+This code takes an integer from arguments and
+calculates the Nth Fibonacci number using 3 approches
+and logs their excecution times
+
+The three approaches are:
 
 Iteratively with memoization,
 Recursively with memoization,
 and the naive recursive solution
 */
 
+type FibFunc func(int) int
+
+// Decerator for logging the excecution time of the Fibonacci algorithms
+func excecutionTimeLogger(f FibFunc) FibFunc {
+	return func(n int) int {
+		defer func(t time.Time) {
+			fmt.Printf("%s - runtime: %d\n", runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name(), time.Since(t))
+		}(time.Now())
+		return f(n)
+	}
+}
+
 func main() {
-	memo := make(map[int]int)
 	n, _ := strconv.Atoi(os.Args[1])
 
-	fmt.Println(recursiveFibDP(n, memo))
-	fmt.Println(iterativeFibDP(n))
-	fmt.Println(fibNaive(n))
+	excecutionTimeLogger(recursiveFibDP)(n)
+	excecutionTimeLogger(iterativeFibDP)(n)
+	excecutionTimeLogger(fibNaive)(n)
 }
 
 // Iteratively return the Nth Fibonacci number utilizing memoization!!
@@ -44,7 +61,7 @@ func iterativeFibDP(n int) int {
 // Recursively returns the Nth Fibonacci number utilizing memoization!!
 // Time  complexity: O(n)
 // Space complexity: O(n)
-func recursiveFibDP(n int, memo map[int]int) int {
+func RecursiveFibDP(n int, memo map[int]int) int {
 	// Check if the value is in the memo
 	value, found := memo[n]
 	if found {
@@ -53,11 +70,17 @@ func recursiveFibDP(n int, memo map[int]int) int {
 	if n <= 1 {
 		value = n
 	} else {
-		value = recursiveFibDP(n-1, memo) + recursiveFibDP(n-2, memo)
+		value = RecursiveFibDP(n-1, memo) + RecursiveFibDP(n-2, memo)
 	}
 
 	memo[n] = value
 	return value
+}
+
+// Helper function
+func recursiveFibDP(n int) int {
+	memo := make(map[int]int)
+	return RecursiveFibDP(n, memo)
 }
 
 // Naive solution:
